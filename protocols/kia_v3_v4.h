0bbb58a9f0113f378f7d752dfb10f57fcf38b247
limbo111111 <228673403+limbo111111@users.noreply.github.com>
2025-12-17 04:29:13 +0100

    Aktualisieren von kia_v3_v4.h

--- protocols/kia_v3_v4.h
+++ protocols/kia_v3_v4.h
@@ -1,9 +1,14 @@
 #pragma once
 
-#include "kia_generic.h"
+#include <lib/subghz/protocols/base.h>
 
 #define KIA_PROTOCOL_V3_V4_NAME "Kia V3/V4"
 
+typedef void SubGhzProtocolDecoderKiaV3V4;
+typedef void SubGhzProtocolEncoderKiaV3V4;
+
+extern const SubGhzProtocolDecoder kia_protocol_v3_v4_decoder;
+extern const SubGhzProtocolEncoder kia_protocol_v3_v4_encoder;
 extern const SubGhzProtocol kia_protocol_v3_v4;
 
 void* kia_protocol_decoder_v3_v4_alloc(SubGhzEnvironment* environment);
@@ -15,12 +20,15 @@ SubGhzProtocolStatus kia_protocol_decoder_v3_v4_serialize(
     void* context,
     FlipperFormat* flipper_format,
     SubGhzRadioPreset* preset);
-SubGhzProtocolStatus
-    kia_protocol_decoder_v3_v4_deserialize(void* context, FlipperFormat* flipper_format);
+SubGhzProtocolStatus kia_protocol_decoder_v3_v4_deserialize(
+    void* context,
+    FlipperFormat* flipper_format);
 void kia_protocol_decoder_v3_v4_get_string(void* context, FuriString* output);
 
 void* kia_protocol_encoder_v3_v4_alloc(SubGhzEnvironment* environment);
 void kia_protocol_encoder_v3_v4_free(void* context);
-SubGhzProtocolStatus kia_protocol_encoder_v3_v4_deserialize(void* context, FlipperFormat* flipper_format);
-void kia_protocol_encoder_v3_v4_yield(void* context);
+SubGhzProtocolStatus kia_protocol_encoder_v3_v4_deserialize(
+    void* context,
+    FlipperFormat* flipper_format);
 void kia_protocol_encoder_v3_v4_stop(void* context);
+SubGhzLevelDuration kia_protocol_encoder_v3_v4_yield(void* context);
